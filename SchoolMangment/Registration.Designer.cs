15b2f21d5f7cc3a13a5328405a398d55692f1909
Remi shan <cherukattilremi@gmail.com>
2021-05-22 17:24:13 +0530

    Add project files.

---
+++ SchoolMangment/Registration.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace SchoolMangment
+{
+    partial class Registration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInformation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Salary";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(77, 84);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(123, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(77, 145);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(123, 20);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(77, 184);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(123, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(77, 219);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(123, 20);
+            this.txtSalary.TabIndex = 7;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(67, 256);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(163, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Registration";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(267, 122);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(123, 23);
+            this.btnRead.TabIndex = 18;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(402, 261);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 19;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Location = new System.Drawing.Point(77, 110);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbtnFemale.TabIndex = 21;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Location = new System.Drawing.Point(152, 110);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(48, 17);
+            this.rbtnMale.TabIndex = 22;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(267, 87);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(123, 20);
+            this.txtId.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(223, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Id";
+            // 
+            // txtInformation
+            // 
+            this.txtInformation.Location = new System.Drawing.Point(226, 152);
+            this.txtInformation.Multiline = true;
+            this.txtInformation.Name = "txtInformation";
+            this.txtInformation.Size = new System.Drawing.Size(164, 122);
+            this.txtInformation.TabIndex = 25;
+            this.txtInformation.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Registration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 308);
+            this.Controls.Add(this.txtInformation);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.rbtnMale);
+            this.Controls.Add(this.rbtnFemale);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Registration";
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInformation;
+    }
+}
+
---
+++ SchoolMangment/Registration.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SchoolMangment
+{
+    public partial class Registration : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numberoflist();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string gender;
+            if(rbtnFemale.Checked==true)
+            {
+                gender = "F";
+
+            }
+            else
+            {
+                gender = "M";
+            }
+            SqlConnection connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "PlayerData";
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Name", txtName.Text);
+            command.Parameters.AddWithValue("Gender",gender);
+            command.Parameters.AddWithValue("Email",txtEmail.Text);
+            command.Parameters.AddWithValue("Phone", txtPhone.Text);
+            command.Parameters.AddWithValue("Salary", Convert.ToInt32(txtSalary.Text));
+            command.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Done");
+
+            numberoflist();
+            txtName.Clear();
+            txtPhone.Clear();
+            txtSalary.Clear();
+            txtEmail.Clear();
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string teamId="";
+            string name="";
+            string gender="";
+            string Phone="";
+            string email="";
+            string salary="";
+            if (txtId.Text=="")
+            {
+                MessageBox.Show("please enter id");
+            }
+            else
+            {
+                connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "PlayerList";
+                command.CommandType = CommandType.StoredProcedure;
+               // command.Parameters.AddWithValue("Id", txtId.Text);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                       teamId = reader["Id"].ToString();
+                     name = reader["Name"].ToString();
+                     gender = reader["Gender"].ToString();
+                     Phone = reader["Phone"].ToString();
+                     email = reader["Email"].ToString();
+                     salary = reader["Salary"].ToString();
+                    if (txtId.Text == teamId)
+                        break;
+                }
+                if (txtId.Text == teamId)
+                {
+                    txtInformation.AppendText("User Information" + "\r\n" + ".............." + "\r\n" + "Name:" + name + "\r\n" + "Gender:" + gender + "\r\n" + "Phone number:" + Phone + "\r\n" + "email id:" + email + "\r\n" + "Salary:" + salary);
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Not Found");
+                }
+               // txtInformation.Clear();
+
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void numberoflist()
+        {
+            connection = new SqlConnection(@"Server=DESKTOP-C0MLP28\SQLEXPRESS;Database=CricketTeamManagement;Trusted_Connection=true;");
+            connection.Open();
+            command = connection.CreateCommand();
+            command.CommandText = "TeamCount";
+            command.CommandType = CommandType.StoredProcedure;
+          int count=Convert.ToInt32( command.ExecuteScalar());
+            lblCount.Text = count.ToString();
+        }
+    }
+}
